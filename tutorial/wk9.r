8a676ec021ddc6b551b977744f9f02c5b4b4f698
Nick Spyrison (Acer laptop) <spyrison@gmail.com>
2022-09-21 13:15:54 +1000

    week 9

---
+++ tutorial/wk9.r
@@ -0,0 +1,20 @@
+# Overview ----
+
+# - Last week we covered Factor Modelling
+# - This week's focus: 
+
+# This week ----
+
+# 1) Discuss the Concepts section.
+# 2) Work through Application as groups 15 min),
+#    please ask questions & seek help.
+# 3) Discuss and compare against solutions.
+
+
+# Content ----
+
+## See slides from this weeks 7-9 as needed.
+##
+## The tutorial today is more theory than application in R.
+##
+## Give it a go then we'll work through it together.
